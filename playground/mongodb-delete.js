27fa8655d5c9764ea90d9e131d72c1c95b4f784f
joshuascott81 <joshuascott81@gmail.com>
2018-02-23 19:31:13 -0600

    Added ..delete.js and ..update.js and removed promise-2.js

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,27 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  const db = client.db('TodoApp');
+  console.log('Connected to MongoDB server');
+
+  //deleteMany
+  // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  //deleteOne
+  // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  //findOneAndDelete
+  db.collection('Todos').findOneAndDelete({_id: new ObjectID('5a90538030883e73f55a5ff2')}).then((result) => {
+    console.log(result);
+  });
+
+  //client.close();
+});
